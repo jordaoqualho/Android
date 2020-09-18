149f3fa7a62f100e0621290b1ab46b6a0ec76792
jordaoqualho <50970557+jordaoqualho@users.noreply.github.com>
2020-09-18 00:30:51 -0300

    Adicionando telas

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -3,15 +3,17 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.button
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        button.setOnClickListener{
-            startActivity(Intent(this, LoginActivity::class.java))
+        back.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
